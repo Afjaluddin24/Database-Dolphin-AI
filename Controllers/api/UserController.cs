7df6d9fc59d97551bbefd2d60f399079435941bb
Afjaluddin Shekh <shekhafjal024@gmail.com>
2026-03-04 17:21:10 +0530

    fix image folder issue

--- Controllers/api/UserController.cs
+++ Controllers/api/UserController.cs
@@ -207,7 +207,7 @@ namespace Dolphin_AI.Controllers.api
 
         }
 
-        [HttpDelete("remove/{Id}")]
+        [HttpPost("remove/{Id}")]
 
         public async Task<ActionResult> remove(int? Id)
         {
