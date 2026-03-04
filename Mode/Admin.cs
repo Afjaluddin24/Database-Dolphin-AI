11dc3f6b31be35eda0836ad56b812265c2c7ee6f
Afjaluddin Shekh <shekhafjal024@gmail.com>
2026-03-04 14:37:51 +0530

    fix image folder issue

--- Mode/Admin.cs
+++ Mode/Admin.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
 
 namespace Dolphin_AI.Mode
 {
@@ -7,21 +6,25 @@ namespace Dolphin_AI.Mode
     {
         [Key]
         public int AdminId { get; set; }
+
         public string? name { get; set; }
         public string? fullname { get; set; }
 
-        [Required(ErrorMessage ="email is required")]
+        [Required(ErrorMessage = "email is required")]
         [EmailAddress]
-        public string? email { get; set; }
+        public string email { get; set; }
+
         public string? phoneno { get; set; }
         public string? Adress { get; set; }
 
         [Required(ErrorMessage = "password is required")]
-        public string? password { get; set; }
+        public string password { get; set; }
+
         public string? Logo { get; set; }
         public string? Baner { get; set; }
         public string? Mapurl { get; set; }
-        public DateTime? created_at { get; set; } = DateTime.UtcNow;
+
+        public DateTime created_at { get; set; } = DateTime.UtcNow;
     }
 
     public class Authenticationa
@@ -49,6 +52,4 @@ namespace Dolphin_AI.Mode
         public string? Baner { get; set; }
         public string? Mapurl { get; set; }
     }
-
-
 }
